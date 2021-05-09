3ccb8ebf9a30b9e432d1ab148767076b99b6f4c3
schuklin_pavel <ininin@vivaldi.net>
2021-05-09 22:11:35 +0400

    add getErrorExit

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import minimist from 'minimist';
 
 import {CaesarCipherTransform} from "./utils/Streams.js";
 import {checkIncomingParams, checkShift} from "./utils/checkIncoming.js";
+import { getErrorExit } from "./utils/getError.js";
 
 const args = minimist(process.argv.slice(2), {
     alias: {
@@ -24,11 +25,11 @@ const oldOutput = fs.readFileSync(output, 'utf8');
 export const run = async () => {
     await pipeline(
         input
-            ? fs.createReadStream(input).on('error', () => console.error('Ошибка открытия файла'))
+            ? fs.createReadStream(input).on('error', () => getErrorExit('Ошибка открытия файла'))
             : process.stdin,
         new CaesarCipherTransform({action, shift, oldOutput}),
         output
-            ? fs.createWriteStream(output).on('error', () => console.error('Ошибка записи файла'))
+            ? fs.createWriteStream(output).on('error', () => getErrorExit('Ошибка записи файла'))
             : process.stdout,
     )
 }
